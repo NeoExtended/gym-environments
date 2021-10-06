fd4c42dd869564329b58790b6a7925296cbcdbaa
Matthias Konitzny <konitzny@ibr.cs.tu-bs.de>
2021-10-06 16:55:30 +0200

    Improved imports.

--- gym_maze/wrappers/__init__.py
+++ gym_maze/wrappers/__init__.py
@@ -2,3 +2,4 @@ from gym_maze.wrappers.wrappers import (
     FakeParticleNoiseWrapper,
     InvisibleParticleNoiseWrapper,
 )
+from gym_maze.wrappers.sample_hard_goals import VecHardGoalSampleWrapper
