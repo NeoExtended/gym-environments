17ca6619a241e19b3dc880cb3133e35a2c25b762
Matthias Konitzny <konitzny@ibr.cs.tu-bs.de>
2021-10-07 17:43:36 +0200

    Small naming fix

--- gym_environments/gravity_packing/__init__.py
+++ gym_environments/gravity_packing/__init__.py
@@ -4,5 +4,5 @@ from gym.envs.registration import register
 register(
     id="GravityUnloading-v0",
     entry_point="gym_environments.gravity_packing.gravity_env:UnloadingEnvironment",
-    max_episode_length=200,
+    max_episode_steps=200,
 )
